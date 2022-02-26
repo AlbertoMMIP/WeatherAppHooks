4db3f5e6778324556d4ad249f1b3258152f6caf0
Alberto Martinez <alberto@intelligential.tech>
2022-02-26 12:17:39 -0600

    [FEAT] Implement Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router,
+  Switch,
+  Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1>App</h1>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            Welcom
+          </Route>
+          <Route path='/main'>
+            Main
+          </Route>
+          <Route path='/city'>
+            City
+          </Route>
+          <Route>
+            Not found
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   )
 }
 
