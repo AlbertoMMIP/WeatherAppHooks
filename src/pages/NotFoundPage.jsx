c64b9b46afacf08e877e186fd0130a3771a22682
Alberto Martinez <alberto@intelligential.tech>
2022-03-05 12:44:13 -0600

    [FEAT] Style to 404 page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Grid, Link, Typography } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import { IconContext } from 'react-icons';
+import { WiRain } from 'react-icons/wi';
 
 const NotFound = () => {
   return (
-    <div>
-      Not found
-      <div>
-        <Link to='/main'>Ir a Main</Link>
-      </div>
-    </div>
+    <Grid container
+        direction='column'
+        justifyContent='center'
+        className='full'>
+          <div className="highlight">
+            <Grid item container xs={12}
+              justifyContent='center'
+              alignItems='center'>
+                <Grid item>
+                  <IconContext.Provider value={{ size:'6em'}}>
+                    <WiRain />
+                  </IconContext.Provider>
+                </Grid>
+                <Grid item
+                  container
+                  direction='column'
+                  justifyContent='center'
+                  alignItems='center'>
+                    <Typography variant='h4' color='inherit'>
+                      404 | La página no existe
+                    </Typography>
+                    <Link color='inherit'
+                      aria-label='menu'
+                      component={RouterLink}
+                      to='/'>
+                        Velver al inicio
+                    </Link>
+                </Grid>
+            </Grid>
+          </div>
+      </Grid>
   )
 }
 
