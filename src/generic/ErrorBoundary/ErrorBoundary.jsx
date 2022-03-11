1778a637c2584c18f9f6fc244fe17de51a6ec7ff
Alberto Martinez <alberto@intelligential.tech>
2022-03-11 13:56:21 -0600

    [FEAT] Class component example

---
+++ src/generic/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      activo: false
+    }
+  }
+  
+  isActive = () => {
+    return this.state.activo ? 'Active' : 'No Activo'
+  }
+
+  onClickHandler = () => {
+    this.setState({ activo: !this.state.activo })
+  }
+
+  componentDidMount() { 
+   console.log('El componente se ha montado'); 
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prevState', prevState.activo);
+    console.log('curreState', this.state.activo);
+    console.log('El componente se ha actualizado');  
+  }
+
+  componentWillUnmount() {
+    console.log('El component se ha desmontado');
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onClickHandler}>
+          Activar
+        </button>
+        <h1>
+          ErrorBoundary {this.props.saludo} 
+          {
+            this.isActive()
+          }
+        </h1>
+      </div>
+    )
+  }
+
+}
+
+export default ErrorBoundary;
---
+++ src/generic/ErrorBoundary/ErrorBoundary.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ErrorBoundary from './ErrorBoundary';
+
+export default {
+  title: 'Error Boundary',
+  component: ErrorBoundary
+}
+
+export const ErrorBoundaryExample = () => <ErrorBoundary saludo='Hi myfriend' />
