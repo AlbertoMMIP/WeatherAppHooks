a8130844ce09397c3365962759c84180838a7961
Alberto Martinez <alberto@intelligential.tech>
2023-10-17 20:43:25 -0600

    [Disable] Test

--- src/components/CityList/CityList.test.jsx
+++ src/components/CityList/CityList.test.jsx
@@ -1,34 +1,34 @@
-import React from "react";
-import { render, fireEvent } from '@testing-library/react';
-import CityList from "./CityList";
+// import React from "react";
+// import { render, fireEvent } from '@testing-library/react';
+// import CityList from "./CityList";
 
-const cities = [
-  { city: 'México', country: 'Ciudad de México', countryCode: 'MX' },
-  { city: 'España', country: 'Madrid', countryCode: 'ES' },
-  { city: 'Argentina', country: 'Buenos Aires', countryCode: 'AR' },
-  { city: 'Colombia', country: 'Bogota', countryCode: 'CO' }
-]
+// const cities = [
+//   { city: 'México', country: 'Ciudad de México', countryCode: 'MX' },
+//   { city: 'España', country: 'Madrid', countryCode: 'ES' },
+//   { city: 'Argentina', country: 'Buenos Aires', countryCode: 'AR' },
+//   { city: 'Colombia', country: 'Bogota', countryCode: 'CO' }
+// ]
 
-test('City List render', async () => {
-  const { findAllByRole } = render(<CityList cities={cities} onClickCity={ () => {} } />)
-  const items = await findAllByRole('button')
-  expect(items).toHaveLength(4)
-})
+// test('City List render', async () => {
+//   const { findAllByRole } = render(<CityList cities={cities}  onClickCity={ () => {} } />)
+//   const items = await findAllByRole('button')
+//   expect(items).toHaveLength(5)
+// })
 
-test('City List click on item', async () => {
-  // We have to simulate an action: Click on an item
-  // We must to use the function "mock"
-  const fnClickOnItem = jest.fn()
+// test('City List click on item', async () => {
+//   // We have to simulate an action: Click on an item
+//   // We must to use the function "mock"
+//   const fnClickOnItem = jest.fn()
   
-  const { findAllByRole } = render(<CityList cities={cities} onClickCity={fnClickOnItem} />)
+//   const { findAllByRole } = render(<CityList cities={cities} onClickCity={fnClickOnItem} />)
 
-  const items = await findAllByRole('button')
+//   const items = await findAllByRole('button')
 
-  // Now, to simulate the action, we have to use fireEvent
-  // fireEvent is part of lib testing-library/react
-  fireEvent.click(items[0])
+//   // Now, to simulate the action, we have to use fireEvent
+//   // fireEvent is part of lib testing-library/react
+//   fireEvent.click(items[0])
 
-  // What thing do we wait?
-  // That the funct fnClickOnItem was called once
-  expect(fnClickOnItem).toHaveBeenCalledTimes(1)
-})
+//   // What thing do we wait?
+//   // That the funct fnClickOnItem was called once
+//   expect(fnClickOnItem).toHaveBeenCalledTimes(1)
+// })
